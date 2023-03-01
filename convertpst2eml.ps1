eabc09abf04caae0b782b363a8565f0d2bc56f38
Anh Le <anhle03@gmail.com>
2023-03-01 10:49:19 -0600

    new convert script

--- convertpst2eml.ps1
+++ convertpst2eml.ps1
@@ -9,3 +9,17 @@ $folder = $inbox.Folders | where {$_.Name -eq "YourFolderName"}
 $folder.Items | foreach {
   $_.SaveAs("C:\EMLFiles\" + $_.Subject + ".eml", 17)
 }
+
+
+Add-Type -AssemblyName Microsoft.Office.Interop.Outlook
+
+$pstPath = "C:\Path\To\PST\File.pst"
+$outputFolder = "C:\Path\To\Output\Folder"
+
+$outlook = New-Object -ComObject Outlook.Application
+$namespace = $outlook.GetNameSpace("MAPI")
+$inbox = $namespace.OpenStore($pstPath).GetRootFolder()
+
+$inbox.Items | foreach {
+    $_.SaveAs(($outputFolder + "\" + $_.Subject + ".eml"), 17)
+}
